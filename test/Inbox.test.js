95ad4f9c10cfc94fd7460e0ae802e7edbb37469a
writes <jonathonhthompson@gmail.com>
2018-02-18 18:29:17 -0600

    feat(test): update test script

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,5 @@
 const assert = require('assert');
-const ganache = requre('ganache-cli');
+const ganache = require('ganache-cli');
 const Web3 = require('web3');
+// Web3 instance with provider to talk to ganache
+const web3 = new Web3(ganache.provider());
