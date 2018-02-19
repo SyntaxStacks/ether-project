8ebe1950b93840e47114e0ff8287e83e81b31feb
writes <jonathonhthompson@gmail.com>
2018-02-19 14:09:43 -0600

    feat(test): test deploy

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,36 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-// Web3 instance with provider to talk to ganache
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+
+// Web3 instance and provider for ganache
+const web3 = new Web3(provider);
+
+// import interface and bytecode from compiled contract
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // list accounts
+  accounts = await web3.eth.getAccounts();
+
+  // use accounts to deploy contract
+  // represents what exists on chain
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi There!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    // assert the value exists
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+});
